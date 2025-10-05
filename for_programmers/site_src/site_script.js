88d098b505951753498dc22875d80e6035e08972
Sergey Bulatov <ssergeybulatov1435@gmail.com>
2025-10-05 18:08:39 +0300

    ForProgrammer fix

---
+++ for_programmers/site_src/site_script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+function test_working() {
+    console.log("site_script.js загружен!");
+}
+test_working();
+
+
+class ProgrammerGuide {
+    /*
+    Основной класс для сайта.
+    */
+
+    constructor() {
+        console.log("ProgrammerGuide работает!");
+    }
+}
+
+
+function main() {
+    console.log("Вам не нужно запускать этот файл!")
+}
